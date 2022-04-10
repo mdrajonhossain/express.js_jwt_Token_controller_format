289e5a1d3e19e19b94b5247cc14a5ad276a7a1ef
rajonhossain <rajonhossaindhaka@gmail.com>
2022-04-10 21:19:05 +0600

    testt

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 const user_controller = require('../controller/user_controller');
 const authverifyface = require('../middleware/auth_middleware');
+require('dotenv').config();
 
 
 
@@ -12,15 +13,12 @@ router.get('/', user_controller.root);
  
 
 router.post('/login', function(req, res, next){
-
 	  const token = jwt.sign({
 	  		"username": req.body.username,
 			"password": req.body.password
-	  	}, "123456abcdefg", {
+	  	}, process.env.JWT_SECTRET, {
 	  		expiresIn:'10h'
 	  	});
-
-
 
 	    return res.json({
 	     	"username": req.body.username,
@@ -34,21 +32,22 @@ router.post('/login', function(req, res, next){
 
 
 const isAuthenticate = (req, res, next)=>{
-	const { authorization } = req.headers;	
+	const { authorization } = req.headers;		
 	try {
 		if(authorization){
 			const token = authorization.split(' ')[1];
-	  		var decoded = jwt.verify(token, '123456abcdefg');	  		
-	  		req.username = "Md. Rajon Hossain";
-	  		req.password = "password";  		
-	  		req.token = authorization;  		
-	  		req.message = "Success";
+	  		var decoded = jwt.verify(token, process.env.JWT_SECTRET);
+	  		var { username, password} = decoded;
+	  		req.username = username;
+	  		req.password = password;
+	  		req.token = authorization;
+	  		req.status = "Success";
 	  		next();
-  		}else{
-  			return res.json({"Status": "authorization Null"})
+  		}else{  			
+  			return res.json({"Status": "Unauthorization"})
   		}
-	} catch(error) {		
-  		return res.json({"Status": "Faild"})
+	}catch(error) {		
+  		return res.json({"Status": "Authorization Not Match"})
 	}
 };
 
@@ -57,10 +56,10 @@ const isAuthenticate = (req, res, next)=>{
 
 
 router.post('/test', isAuthenticate, function(req, res, next){
-
 		return res.json({
-			"Status": req.message,
-			"Username": req.username			
+			"Status": req.status,
+			"Username": req.username,
+			"password" : req.password
 		})
 
 })
